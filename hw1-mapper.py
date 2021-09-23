caaf63ada27dea8ca67155ee2a0704bd12ff84eb
Palden Lama <lamapalden@gmail.com>
2021-09-22 19:54:10 -0500

    bug fix

--- hw1-mapper.py
+++ hw1-mapper.py
@@ -3,10 +3,9 @@ from csv import reader
 import sys
 # skip first line (the header)
 next(sys.stdin)
-
 for line in reader(sys.stdin):
-    boro, crime = (line[13].strip(), line[7].strip())
-    if not boro or not crime:
-        continue
-    print ("%s\t%s" % (boro, crime))
+   boro, crime = (line[13].strip(), line[7].strip())
+   if not boro or not crime:
+      continue
+   print ("%s\t%s" % (boro, crime))
 
--- hw1-reducer.py
+++ hw1-reducer.py
@@ -5,27 +5,27 @@ Max_Addr = None; Max_Count = 0
 MaxCrimeList = []; CrimeList = []
 
 for line in sys.stdin:
-    line=line.strip()
-    addr, crime = line.split("\t") 
-    if addr == Cur_Addr: 
-        Cur_Count +=1 
-        CrimeList.append(crime)
-    else:
-        if Cur_Count > Max_Count:
-            Max_Count = Cur_Count
-            Max_Addr = Cur_Addr		
-            MaxCrimeList = CrimeList
-        Cur_Addr = addr
-        Cur_Count = 1
-        CrimeList = []
-        CrimeList.append(crime)
+     line=line.strip()
+     addr, crime = line.split("\t") 
+     if addr == Cur_Addr: 
+         Cur_Count +=1 
+         CrimeList.append(crime)
+     else:
+         if Cur_Count > Max_Count:
+             Max_Count = Cur_Count
+             Max_Addr = Cur_Addr		
+             MaxCrimeList = CrimeList
+         Cur_Addr = addr
+         Cur_Count = 1
+         CrimeList = []
+         CrimeList.append(crime)
+
 
 # after end of for loop
 if Cur_Count > Max_Count:
     Max_Count = Cur_Count
-    Max_Addr = Cur_Addr
+    Max_Addr = Cur_Addr		
     MaxCrimeList = CrimeList
 
-print ("%s\t%s" % (Max_Addr, MaxCrimeList))
-
+print ("%s\t%s" % (Max_Addr, len(MaxCrimeList)))
 
